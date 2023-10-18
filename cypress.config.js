4be8d0095c256a62d179b8785563276a57a444a6
Surya Menon <dev4@sprktechnologies.in>
2023-10-18 15:18:04 +0530

    Test case scripts for AddEnquiry Api call
    -Changed token of Token_admin as the token gets invalid after 24 hours
    -Created new file under Enquiry folder of post method
    -Created fixture file name enquiry-body which will store the body of the enquiry registration body according to many test case scenarios

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
 
   env: {
     TOKEN_DEFAULT: `HALO`,
-    TOKEN_ADMIN: `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidXB0IjoxNjk2NDgzMDE0Mjg5LCJpYXQiOjE2OTcyMDMxNDQsImV4cCI6MTY5NzI4OTU0NH0.SIVl8GBjl8SCx_rJa_yomw_YB_htzWGFWiI8lFZgmWXEfMYYxNXCpR62qaYTkJ3-2ZkoXbwXodm4oCEVZpIXmA`,
+    TOKEN_ADMIN: `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidXB0IjoxNjk2NDgzMDE0Mjg5LCJpYXQiOjE2OTc2MDU1NjEsImV4cCI6MTY5NzY5MTk2MX0.jPHMSVDeWLJpvIAvv4rtoeGcVigF4EhIl6f1LOod8fqO6hELkKgzbG5cao2ldBYEb7ZY71rm1eWmJlA85SmLzg`,
     TOKEN_SALES: `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwidXB0IjoxNjg4MTEyNDc0MDc3LCJpYXQiOjE2OTcyMDMyMDMsImV4cCI6MTY5NzI4OTYwM30.Y2i5pBcgVn1DPKiOXJgSydEvmRY4KkD4cvrrtToeoO-X92zhskwu0WLJO-jEzp34jsTrvHL3flT5pJkJbYmLfw`,
     BASE_URL: `https://mollusk-heroic-jaguar.ngrok-free.app`
   },
---
+++ cypress/e2e/api/enquiry-service/POST/addEnquiry.cy.js
@@ -0,0 +1,77 @@
+const ENV_BASE_URL = Cypress.env("BASE_URL")
+const ENV_TOKEN = Cypress.env("TOKEN_ADMIN")
+
+const API_URL = `${ENV_BASE_URL}/api/enquiry/add`
+const TOKEN = `${ENV_TOKEN}`
+const HttpMethod = {
+    GET: "GET",
+    POST: "POST",
+    PATCH: "PATCH",
+    DELETE: "DELETE"
+}
+
+
+//FIXTURE
+var payload_register = null
+before(() => {
+
+    cy.fixture("enquiry-body").then((data) => {
+        payload_register = data
+    })
+
+})
+
+// DESCRIPTION
+describe('API-addEnquiry Test', () => {
+
+    // TEST
+    it('Create - Enquiry', () => {
+
+        payload_register.forEach((testCase) => {
+
+            let bodyPayload = {
+                student_firstname: testCase.student_firstname,
+                student_middlename: testCase.student_middlename,
+                student_lastname: testCase.student_lastname,
+                referral_firstname: testCase.referral_firstname,
+                referral_middlename: testCase.referral_middlename,
+                referral_lastname: testCase.referral_lastname,
+                birth_date: testCase.birth_date,
+                gender: testCase.gender,
+                residence_no: testCase.residence_no,
+                flat_house_building: testCase.flat_house_building,
+                area_street_sector_village: testCase.area_street_sector_village,
+                landmark: testCase.landmark,
+                pincode: testCase.pincode,
+                town_city: testCase.town_city,
+                state: testCase.state,
+                country: testCase.country,
+                primary_mobile: testCase.primary_mobile,
+                secondary_mobile: testCase.secondary_mobile,
+                primary_email: testCase.primary_email,
+                secondary_email: testCase.secondary_email,
+                qualification: testCase.qualification,
+                occupation: testCase.occupation,
+                college_name: testCase.college_name,
+                career_requirement: testCase.career_requirement,
+                software_known: testCase.software_known,
+                course_history: testCase.course_history,
+                heard_from: testCase.heard_from,
+            }
+
+            // REQUEST
+            cy.request({
+                method: HttpMethod.POST,
+                url: API_URL,
+                failOnStatusCode: false,
+                body: bodyPayload,
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(testCase.expected_status_code)
+            }) // request
+
+        }) // forEach
+
+    }) })
