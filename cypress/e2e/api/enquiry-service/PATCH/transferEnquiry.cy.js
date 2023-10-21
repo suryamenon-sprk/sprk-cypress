1a7f8ceded3a1a6904061108a6996bb38b011ab8
Surya Menon <dev4@sprktechnologies.in>
2023-10-21 17:25:07 +0530

    Test scripts for transferEnquiry

---
+++ cypress/e2e/api/enquiry-service/PATCH/transferEnquiry.cy.js
@@ -0,0 +1,87 @@
+import jwtDecode from "jwt-decode"
+const ENV_BASE_URL = Cypress.env("BASE_URL")
+const ENV_TOKEN_ADMIN = Cypress.env("TOKEN_ADMIN")
+const ENV_TOKEN_SALES = Cypress.env("TOKEN_SALES")
+
+
+const API_URL = `${ENV_BASE_URL}/api/enquiry/transfer`
+const API_URL_ENQ = `${ENV_BASE_URL}/api/enquiry/1`
+const TOKEN_ADMIN = `${ENV_TOKEN_ADMIN}`
+const TOKEN_SALES = `${ENV_TOKEN_SALES}`
+const HttpMethod = {
+    GET: "GET",
+    POST: "POST",
+    PATCH: "PATCH",
+    DELETE: "DELETE"
+}
+
+var payload_register = null
+before(() => {
+
+    cy.fixture("EnquiryBody/discard-body").then((data) => {
+        payload_register = data
+    })
+
+})
+
+describe('API-transferEnquiry Test', () => {
+
+    // TEST
+    it('Transfer - Enquiry', () => {
+
+        payload_register.forEach((testCase) => {
+
+            let bodyPayload = {
+                enquiry_id: testCase.enquiry_id,
+                discard_reason: testCase.discard_reason
+            }
+
+            // REQUEST
+            cy.request({
+                method: HttpMethod.PATCH,
+                url: API_URL,
+                failOnStatusCode: false,
+                body: bodyPayload,
+                headers: {
+                    Authorization: `Bearer ${TOKEN_ADMIN}`
+                }
+            }).then((response) => {
+                console.log(response)
+                expect(response.status).to.equal(testCase.expected_status_code)
+            }) // request
+
+        }) // forEach
+        it('Transfer - Enquiry', () => {
+
+        payload_register.forEach((testCase) => {
+
+            let bodyPayload = {
+                enquiry_id: testCase.enquiry_id,
+                discard_reason: testCase.discard_reason
+            }
+
+            // REQUEST
+            cy.request({
+                method: HttpMethod.PATCH,
+                url: API_URL,
+                failOnStatusCode: false,
+                body: bodyPayload,
+                headers: {
+                    Authorization: `Bearer ${TOKEN_SALES}`
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(testCase.expected_status_code)
+                let responseBody = response.body
+
+                const decodedToken = jwtDecode(TOKEN_SALES);
+                const userId = decodedToken.sub;
+
+                responseBody.forEach(obj => {
+                    expect(obj.assigned_user.user_id).to.equal(Number(userId))
+                });
+            }) // request
+
+        })
+
+    }) }) 
+})
