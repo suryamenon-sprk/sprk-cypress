9300f4efb5b02db40687b478e59f4885d45ac37b
Surya Menon <dev4@sprktechnologies.in>
2023-10-24 22:51:09 +0530

    test script for delete booking

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   env: {
     TOKEN_DEFAULT: `HALO`,
     TOKEN_ADMIN: `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidXB0IjoxNjk4MDUyMDEyMjk0LCJpYXQiOjE2OTgxNTgxMTgsImV4cCI6MTY5ODI0NDUxOH0.usqlxQLYqpqb5r_6usFMKlQqZAcOK49DujeJpoDjYUqHRf-DYnqhwL0dMiniYCvlez_-lPI5DeGCZc6n3JdSGQ`,
-    TOKEN_SALES: `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwidXB0IjoxNjk4MDUyMDEyMjk0LCJpYXQiOjE2OTgwNjM3MDQsImV4cCI6MTY5ODE1MDEwNH0.RQfFuo2XFCy2oyArVKG5mgLMt3bS4i9V_m9-egxaVFzkKZ2t8LWRbmutbH_ZsiNYDFvLmowi1ylDueOeycmu7Q`,
+    TOKEN_SALES: `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwidXB0IjoxNjk4MDUyMDEyMjk0LCJpYXQiOjE2OTgxNjY3MTYsImV4cCI6MTY5ODI1MzExNn0.C2TTNQt5LhdX_NiI6JuxnohSwieccCmuFFiJbtSy7ZOV0q4BqxptSm5nJBEdvwbcOS2Knh5Kiy3minMJu6Tjew`,
     BASE_URL: `https://mollusk-heroic-jaguar.ngrok-free.app`
   },
 
