92c0596be292e9be8383bdf185c132b3a61a7996
Surya Menon <dev4@sprktechnologies.in>
2023-10-25 15:27:29 +0530

    Test script for payment service get apis

---
+++ cypress/e2e/api/payment-service/GET/paymentByStudentId.cy.js
@@ -0,0 +1,102 @@
+const ENV_BASE_URL = Cypress.env("BASE_URL")
+const ENV_TOKEN_ADMIN = Cypress.env("TOKEN_ADMIN")
+const ENV_TOKEN_SALES = Cypress.env("TOKEN_SALES")
+
+
+const API_URL = `${ENV_BASE_URL}/api/payment/s/14`
+const AUTH_HEADER = {
+    Admin: `Bearer ${ENV_TOKEN_ADMIN}`,
+    Sales: `Bearer ${ENV_TOKEN_SALES}`
+}
+const HttpMethod = {
+    GET: "GET",
+    POST: "POST",
+    PATCH: "PATCH",
+    DELETE: "DELETE"
+}
+
+
+
+
+
+//DESCRIPTION
+describe('GET -  PAYMENT - BY STUDENT ID (AllUsers)', () =>{
+
+    it('Authorization Header - No Value',
+        () => {
+            cy.request({
+                method: HttpMethod.GET,
+                url: API_URL,
+                failOnStatusCode: false,
+                headers: {
+                    "ngrok-skip-browser-warning": true
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(404)
+            })
+        }
+    );
+
+
+
+
+    it('Authorization Header - Invalid Value', 
+        () => {
+            cy.request({
+                method: HttpMethod.GET,
+                url: API_URL,
+                failOnStatusCode: false,
+                headers: {
+                    "Authorization": "HALO",
+                    "ngrok-skip-browser-warning": true
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(400)
+                cy.log(response.body.error)
+            })
+        }
+    );
+
+
+
+
+    it('Authorization Header - superuser (TOKEN)',
+        () => {
+            cy.request({
+                method: HttpMethod.GET,
+                url: API_URL,
+                failOnStatusCode: false,
+                headers: {
+                    Authorization: AUTH_HEADER.Admin,
+                    "ngrok-skip-browser-warning": true
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(200)
+                cy.log(response.body)
+            });
+        }
+    );
+    
+
+
+
+    it('Authorization Header - sales (TOKEN)',
+        () => {
+            cy.request({
+                method: HttpMethod.GET,
+                url: API_URL,
+                failOnStatusCode: false,
+                headers: {
+                    Authorization: AUTH_HEADER.Sales,
+                    "ngrok-skip-browser-warning": true
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(200);
+                cy.log(response.body);
+            });
+        }
+    );
+
+
+
+});
---
+++ cypress/e2e/api/payment-service/GET/receiptById.cy.js
@@ -0,0 +1,138 @@
+const ENV_BASE_URL = Cypress.env("BASE_URL")
+const ENV_TOKEN_ADMIN = Cypress.env("TOKEN_ADMIN")
+const ENV_TOKEN_SALES = Cypress.env("TOKEN_SALES")
+
+// const API_URL_BOOKING = `${ENV_BASE_URL}/api/student/bookings/`
+const AUTH_HEADER = {
+    Admin: `Bearer ${ENV_TOKEN_ADMIN}`,
+    Sales: `Bearer ${ENV_TOKEN_SALES}`
+}
+const HttpMethod = {
+    GET: "GET",
+    POST: "POST",
+    PATCH: "PATCH",
+    DELETE: "DELETE"
+}
+// var enquiry_obj = null
+//             beforeEach(() => {
+//                 cy.request({
+//                 method: HttpMethod.GET,
+//                 url: API_URL_BOOKING,
+//                 failOnStatusCode: false,
+//                 headers: {
+//                     Authorization: AUTH_HEADER.Admin,
+//                     "ngrok-skip-browser-warning": true,
+//                 },
+//                 }).then((response) => {
+//                 expect(response.status).to.equal(200);
+//                 enquiry_obj = response.body;
+//                 });
+//             });
+
+
+
+
+//DESCRIPTION
+describe('GET -  PAYMENT - BY BOOKING ID (AllUsers)', () =>{
+    const baseUrl = `${ENV_BASE_URL}/api/payment/receipt/`;
+    const startingNumber = 1;
+    const endNumber = 35;
+
+    let API_URL;
+    for (let number = startingNumber; number <= endNumber; number++) {
+        API_URL = `${baseUrl}${number}`;
+        console.log(API_URL); 
+
+        it(`Authorization Header - No Value ${number}`,
+            () => {
+            
+                cy.request({
+                    method: HttpMethod.GET,
+                    url: API_URL,
+                    failOnStatusCode: false,
+                    headers: {
+                        "ngrok-skip-browser-warning": true
+                    }
+                }).then((response) => {
+                    expect(response.status).to.equal(404)
+                })
+            }
+        );
+
+
+
+
+        it(`Authorization Header - Invalid Value ${number}`, 
+            () => {
+
+                cy.request({
+                    method: HttpMethod.GET,
+                    url: API_URL,
+                    failOnStatusCode: false,
+                    headers: {
+                        "Authorization": "HALO",
+                        "ngrok-skip-browser-warning": true
+                    }
+                }).then((response) => {
+                    expect(response.status).to.equal(400)
+                    cy.log(response.body.error)
+                })
+            }
+        );
+
+
+
+
+        it(`Authorization Header - superuser (TOKEN) ${number}`,
+            () => {
+                cy.request({
+                    method: HttpMethod.GET,
+                    url: API_URL,
+                    failOnStatusCode: false,
+                    headers: {
+                        Authorization: AUTH_HEADER.Admin,
+                        "ngrok-skip-browser-warning": true
+                    }
+                }).then((response) => {
+                    // if (enquiry_obj === null) {
+                    //     expect(response.status).to.equal(204);
+                    // } else {
+                    //     expect(response.status).to.equal(200);
+                    // }
+                    expect(response.status).to.equal(200)
+                    cy.log(response.body)
+                });
+            }
+        );
+        
+
+
+
+        it(`Authorization Header - sales (TOKEN) ${number}`,
+            () => {
+                cy.request({
+                    method: HttpMethod.GET,
+                    url: API_URL,
+                    failOnStatusCode: false,
+                    headers: {
+                        Authorization: AUTH_HEADER.Sales,
+                        "ngrok-skip-browser-warning": true
+                    }
+                }).then((response) => {
+                    // if (enquiry_obj === null) {
+                    //     expect(response.status).to.equal(204);
+                    // } else {
+                    //     expect(response.status).to.equal(200);
+                    // }
+                    expect(response.status).to.equal(200);
+                    cy.log(response.body);
+                });
+            }
+        );
+
+
+    }
+
+
+
+});
